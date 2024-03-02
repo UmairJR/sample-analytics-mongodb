303c9945cbab0fa8b39a28a65c6f060e6d744792
Umair Shaikh <umairjr1265@gmail.com>
2024-03-02 05:43:05 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,136 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  const [activeCustomers, setActiveCustomers] = useState([]);
+  const [accounts, setAccounts] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+  const [lowAmountAccountsIds, setLowAmountAccountIds] = useState([]);
+  const [distinctProducts, setDistinctProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchActiveCustomers = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/active-customers');
+        setActiveCustomers(response.data);
+        const allAccounts = response.data.map(customer => customer.accounts).flat();
+      setAccounts(allAccounts)
+      } catch (error) {
+        console.error('Error fetching active customers:', error);
+      }
+    };
+
+    fetchActiveCustomers();
+  }, []);
+
+  const getTransactions = async (account_id) => {
+    try {
+      const response = await axios.get(`http://localhost:5000/api/get-transaction?account_id=${account_id}`);
+      setTransactions(response.data.transactions);
+    } catch (error) {
+      console.error('Error fetching transactions:', error);
+    }
+  };
+
+  const fetchLowAmountAccountIds = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/low-amount-accounts', { accounts });
+      
+      const accountIds = response.data;
+      setLowAmountAccountIds(accountIds);
+    } catch (error) {
+      console.error('Error fetching low amount account IDs:', error.message);
+    }
+  };
+
+  const fetchDistinctProducts = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/distinct-products');
+      setDistinctProducts(response.data[0].distinctProducts);
+    } catch (error) {
+      console.error('Error fetching distinct products:', error.message);
+    }
+  };
+
+  console.log(activeCustomers);
+  console.log(transactions);
+  console.log(accounts);
+  console.log(lowAmountAccountsIds);
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <div>
+      <h1>Active Customers</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Accounts</th>
+          </tr>
+        </thead>
+        <tbody>
+          {activeCustomers.map(customer => (
+            <tr key={customer._id}>
+              <td>{customer.name}</td>
+              <td>{customer.address}</td>
+              <td>
+                <ul>
+                  {accounts.map((account, index) => (
+                    <li key={index}>{account} <button onClick={() => getTransactions(account)}>Transactions</button></li>
+                  ))}
+                </ul>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h2>Distinct Products</h2>
+      <button onClick={fetchDistinctProducts}>Fetch Distinct Products</button>
+      {distinctProducts.length > 0 ? (
+        <ul>
+          {distinctProducts.map((product, index) => (
+            <li key={index}>{product}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No distinct products found.</p>
+      )}
+      <h2>Get tx lower than amount 5000: (For Active User) </h2>
+      <button onClick={fetchLowAmountAccountIds}>Get Account Ids</button>
+      {lowAmountAccountsIds.length > 0 && (
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+          Account IDs: [
+          {lowAmountAccountsIds.map((accountId, index) => (
+            <span key={accountId}>
+              {index > 0 ? ', ' : ''}
+              {accountId}
+            </span>
+          ))}
+          ]
         </p>
+      )}
+
+      {transactions.length > 0 && (
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <h2>Transactions - {transactions.length} Transactions</h2>
+          <ul>
+            {transactions.map((transaction, index) => (
+              <li key={index}>
+                <strong>Amount:</strong> {transaction.amount}<br />
+                <strong>Date:</strong> {transaction.date}<br />
+                <strong>Price:</strong> {transaction.price}<br />
+                <strong>Symbol:</strong> {transaction.symbol}<br />
+                <strong>Total:</strong> {transaction.total}<br />
+                <strong>Transaction Code:</strong> {transaction.transaction_code}<br />
+                <hr />
+              </li>
+            ))}
+          </ul>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      )}
+    </div>
   );
-}
+};
+  
+
